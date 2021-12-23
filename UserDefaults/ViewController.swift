6a497559f1731e7aeb79fa7bee02ce1f74371cd7
umut9191 <umutsurmeli91@gmail.com>
2021-12-24 00:59:55 +0300

    added save, delete button, textField, label UI tools and added constraints for designing and added some codes about userDefault registering and reading from registered datas

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var namelabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let data = UserDefaults.standard.object(forKey: "name") as? String{
+            namelabel.text = data
+        }
     }
 
-
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        if sender.currentTitle=="Save" {
+            UserDefaults.standard.setValue(textField.text, forKey: "name")
+        }
+        if sender.currentTitle=="Delete" {
+        
+        }
+    }
+    
 }
 
